32ebeb28c4748c25723ad402a80e7761a1dfa3c4
Edu <edugacv@gmail.com>
2021-01-25 14:38:29 +0100

    refactoritzation of the code, much better done with htelp of Oscar Miras

--- js/app.js
+++ js/app.js
@@ -5,14 +5,13 @@ const app = Vue.createApp({
         return {
             showQCard: false, //boolean to use for showing question form
             btnQ: 'Add Question', //text for the button add question in case we want to change it
+            error: false,
             showFlashcard: false, //boolean to use for showing the flashcard
             flashCards: [], //Array that will contain our flasshcards
             //Our object flashcard
-            flashcard: {
-                question: '',
-                answer: '',
-            },
-            showAnswer: false //boolean to use for swhoing answer
+            question: '',
+            answer: '',
+            editingCard: undefined
         }
     },
 
@@ -20,27 +19,62 @@ const app = Vue.createApp({
         //Method that will change the state of the question card to it's opposite on every click we do to it. Added on line 29 of the html
         showQuestion(){
             this.showQCard = !this.showQCard
+            this.editingCard = undefined
         },
         //Method that activates when we submit the form and shows us the flash card. It makes an unshift to our array with the two values added on the form, plus it adds a random unique ID using nanoid.
         //We add the method on line 42 of the html
         submitForm(){
             this.showFlashcard = true
             //NOTE: Unshift does the same as a push, but it adds the element on the first position of the array
-            this.flashCards.unshift({
-                ...this.flashcard, 
-                id: nanoid(),
-            })
+            if(!this.answer || !this.question){
+                this.error = true
+                return
+            }
+            //If we are on editModem we just have to update
+            if(this.editingCard){
+                this.editingCard.question = this.question
+                this.editingCard.answer = this.answer
+                this.editingCard = undefined
+                this.question = ''
+                this.answer = ''
+                this.error = false
+                this.showQCard = !this.showQCard
+
+                return 
+            }
+            const newCard = {
+                question: this.question,
+                answer: this.answer,
+                showAnswer: false,
+                id: nanoid()
+            }
+            this.flashCards.unshift(newCard)
+            //clean form fields
+            this.question = ''
+            this.answer = ''
+            this.error = false
             this.showQCard = !this.showQCard
         },
-        //Method to toggle the value of showing the answer, related on the line 66 of the html file
-        toggleAnswer(){
-            this.showAnswer = !this.showAnswer
+        //Method to toggle the value of showing the answer, related on the line 67 of the html file
+        toggleAnswer(flashcard){
+            flashcard.showAnswer = !flashcard.showAnswer
         },
         //Method to remove the element of the array related with their unique id. Added on line 70 of the html
         removeCard(id){
             this.flashCards = this.flashCards.filter(elm => {
-                return elm.id != id
+                elm.id != id
             })
+        },
+
+        getCardById(id){
+            return this.flashCards.find(flashcard => {
+                return flashcard.id == id
+            })
+        },
+
+        editCard(flashcard){
+            this.showQCard = !this.showQCard
+            this.editingCard = flashcard //we changed out app status to inform that we are in edition mode, plus we are editting flashcard
         }
     }
 })
