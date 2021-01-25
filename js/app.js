9a0857a94666e588f38fae76b349c7483f9cfc94
Edu <edugacv@gmail.com>
2021-01-25 10:49:24 +0100

    Implemented solution until showin question and toggle answer

--- js/app.js
+++ js/app.js
@@ -3,6 +3,30 @@ import { nanoid } from 'https://cdn.jsdelivr.net/npm/nanoid/nanoid.js'
 const app = Vue.createApp({
     data() {
         return {
+            showQCard: false,
+            btnQ: 'Add Question',
+            showFlashcard: false,
+            flashCards: [],
+            flashcard: {
+                question: '',
+                answer: ''
+            },
+            showAnswer: false
+        }
+    },
+
+    methods: {
+        showQuestion(){
+            this.showQCard = !this.showQCard
+        },
+
+        submitForm(){
+            this.showFlashcard = true
+            this.flashCards.push({...this.flashcard})
+        },
+
+        toggleAnswer(){
+            this.showAnswer = !this.showAnswer
         }
     }
 })
